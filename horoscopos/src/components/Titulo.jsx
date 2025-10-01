e9aaa6eede7ad6f3c9cbff419f830ed45cb6353b
Alda <rochaaldanafcojavier@gmail.com>
2025-10-01 15:43:21 -0600

    Commit Inicial

---
+++ horoscopos/src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Titulo = () => {
+    return(
+        <>
+            <h1 className="titulo">
+                Horóscopo React 
+            </h1>
+        </>
+    );
+}
+
+export default Titulo;
